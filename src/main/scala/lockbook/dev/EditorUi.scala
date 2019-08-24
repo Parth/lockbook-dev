439f0dc16880b3fa5ade4f973cf35e02ec73e433
Parth <parth@mehrotra.me>
2019-08-23 21:00:53 -0400

    autosave

--- src/main/scala/lockbook/dev/EditorUi.scala
+++ src/main/scala/lockbook/dev/EditorUi.scala
@@ -1,13 +1,16 @@
 package lockbook.dev
 
 import java.io.File
+import java.util.concurrent.{ScheduledFuture, ScheduledThreadPoolExecutor, TimeUnit}
 
 import com.vladsch.flexmark.ast._
 import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.util.ast.{Node, NodeVisitor, VisitHandler}
 import javafx.application.Platform
+import javafx.geometry.Pos
 import javafx.scene.control._
-import javafx.scene.layout.{BorderPane, HBox}
+import javafx.scene.layout.BorderPane
+import javafx.scene.text.{Font, FontPosture}
 import org.eclipse.jgit.api.Git
 import org.fxmisc.richtext.CodeArea
 
@@ -18,10 +21,17 @@ import scala.concurrent.Future
 class EditorUi(editorHelper: EditorHelper) {
 
   def getView(git: Git, f: File): BorderPane = {
-    val textArea = new CodeArea
-    val root     = new BorderPane
+    val textArea  = new CodeArea
+    val root      = new BorderPane
+    val syncLabel = new Label("Loading File")
 
     loadFile(git, f, root, textArea)
+
+    BorderPane.setAlignment(syncLabel, Pos.CENTER_RIGHT)
+    syncLabel.setId("SyncStatus")
+    root.bottomProperty().setValue(syncLabel)
+    val autoSaveTask = saveCommitAndPushTask(syncLabel, textArea, f, git)
+    scheduleAutoSave(textArea, syncLabel, autoSaveTask)
 
     root
   }
@@ -32,7 +42,6 @@ class EditorUi(editorHelper: EditorHelper) {
         Platform.runLater(() => {
           doMarkdown(text)
           text.setWrapText(true)
-          root.setBottom(getBottom(git, f, text))
           root.setCenter(text)
           text.replaceText(fileText)
         })
@@ -40,6 +49,36 @@ class EditorUi(editorHelper: EditorHelper) {
         Platform.runLater(
           () => root.setCenter(new Label(error.uiMessage))
         )
+    }
+  }
+
+  private def scheduleAutoSave(text: CodeArea, syncLabel: Label, saveTask: Runnable): Unit = {
+    val executor                                = new ScheduledThreadPoolExecutor(1)
+    var currentTask: Option[ScheduledFuture[_]] = None
+
+    text
+      .textProperty()
+      .addListener((_, _, _) => {
+        syncLabel.setText("")
+        currentTask.foreach(_.cancel(false))
+        currentTask = Some(executor.schedule(saveTask, 1, TimeUnit.SECONDS))
+      })
+  }
+
+  def saveCommitAndPushTask(l: Label, codeArea: CodeArea, file: File, git: Git): Runnable = () => {
+    Platform.runLater(() => l.setText("Syncing..."))
+    Future {
+      editorHelper
+        .saveCommitAndPush("", codeArea.getText, file, git) match {
+        case Right(_) =>
+          Platform.runLater(() => {
+            l.setText("Sync successful")
+          })
+        case Left(exception) =>
+          Platform.runLater(() => {
+            l.setText(s"Push Failed: $exception.uiMessage}")
+          })
+      }
     }
   }
 
@@ -52,30 +91,6 @@ class EditorUi(editorHelper: EditorHelper) {
         val parsed = parser.parse(newText)
         nodeVisitor(text).visit(parsed)
       })
-  }
-
-  private def getBottom(git: Git, file: File, textArea: CodeArea): HBox = {
-    val save          = new Button("Push")
-    val commitMessage = new TextField
-    commitMessage.setPromptText("Commit Message")
-
-    save.setOnAction(_ => {
-      Future {
-        editorHelper
-          .saveCommitAndPush(commitMessage.getText, textArea.getText, file, git) match {
-          case Right(_) =>
-            Platform.runLater(() => {
-              AlertUi.showGood("Push Successful", "Changes saved successfully.")
-            })
-          case Left(exception) =>
-            Platform.runLater(() => {
-              AlertUi.showBad("Push Failed", exception.uiMessage)
-            })
-        }
-      }
-    })
-
-    new HBox(commitMessage, save)
   }
 
   private val nodeVisitor: CodeArea => NodeVisitor = (styledText: CodeArea) =>
@@ -107,7 +122,7 @@ class EditorUi(editorHelper: EditorHelper) {
       setParagraphStyle(styledText, node, "inline")
     }))
 
-  private def setParagraphStyle(styledText: CodeArea, node: Node, style: String) = {
+  private def setParagraphStyle(styledText: CodeArea, node: Node, style: String): Unit = {
     Array
       .range(node.getStartLineNumber, node.getEndLineNumber + 1)
       .foreach(styledText.setParagraphStyle(_, List(style).asJava))
