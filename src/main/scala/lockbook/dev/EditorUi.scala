57145e6114b90e135b5829852a2008559de62391
Parth <parth@mehrotra.me>
2019-10-14 14:00:06 -0400

    dark markdown theme

--- src/main/scala/lockbook/dev/EditorUi.scala
+++ src/main/scala/lockbook/dev/EditorUi.scala
@@ -5,7 +5,7 @@ import java.util.concurrent.{ScheduledThreadPoolExecutor, TimeUnit}
 
 import com.vladsch.flexmark.ast._
 import com.vladsch.flexmark.parser.Parser
-import com.vladsch.flexmark.util.ast.{Document, Node, NodeVisitor, VisitHandler}
+import com.vladsch.flexmark.util.ast.{Document, NodeVisitor, VisitHandler}
 import javafx.application.Platform
 import javafx.geometry.Pos
 import javafx.scene.control._
@@ -14,7 +14,6 @@ import javafx.scene.layout.BorderPane
 import org.eclipse.jgit.api.Git
 import org.fxmisc.richtext.CodeArea
 
-import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
@@ -147,36 +146,26 @@ class EditorUi(editorHelper: EditorHelper, gitHelper: GitHelper, executor: Sched
   private val nodeVisitor: CodeArea => NodeVisitor = (styledText: CodeArea) =>
     new NodeVisitor(new VisitHandler[Text](classOf[Text], (node: Text) => {
       styledText.setStyleClass(node.getStartOffset, node.getEndOffset, "text")
-      setParagraphStyle(styledText, node, "inline")
     }), new VisitHandler[Heading](classOf[Heading], (node: Heading) => {
       styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getText.getEndOffset, s"h${node.getLevel}")
-      setParagraphStyle(styledText, node, "inline")
     }), new VisitHandler[Code](classOf[Code], (node: Code) => {
       styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getClosingMarker.getEndOffset, "code")
-      setParagraphStyle(styledText, node, "inline")
     }), new VisitHandler[Emphasis](classOf[Emphasis], (node: Emphasis) => {
       styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getClosingMarker.getEndOffset, "emphasis")
-      setParagraphStyle(styledText, node, "inline")
     }), new VisitHandler[FencedCodeBlock](classOf[FencedCodeBlock], (node: FencedCodeBlock) => {
       if (node.getClosingFence.getEndOffset != 0) {
         styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getClosingMarker.getEndOffset, "code-block")
-        setParagraphStyle(styledText, node, "code-block")
-
       }
     }), new VisitHandler[BlockQuote](classOf[BlockQuote], (node: BlockQuote) => {
       styledText.setStyleClass(node.getStartOffset, node.getEndOffset, "quote-block")
-      setParagraphStyle(styledText, node, "quote-block")
     }), new VisitHandler[Link](classOf[Link], (node: Link) => {
       styledText
         .setStyleClass(node.getTextOpeningMarker.getStartOffset + 1, node.getTextClosingMarker.getEndOffset - 1, "link")
       styledText
         .setStyleClass(node.getLinkOpeningMarker.getStartOffset + 1, node.getLinkClosingMarker.getEndOffset - 1, "href")
-      setParagraphStyle(styledText, node, "inline")
+    }), new VisitHandler[BulletListItem](classOf[BulletListItem], (node: BulletListItem) => {
+      styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getOpeningMarker.getEndOffset, "bullet")
+    }), new VisitHandler[OrderedListItem](classOf[OrderedListItem], (node: OrderedListItem) => {
+      styledText.setStyleClass(node.getOpeningMarker.getStartOffset, node.getOpeningMarker.getEndOffset, "number")
     }))
-
-  private def setParagraphStyle(styledText: CodeArea, node: Node, style: String): Unit = {
-    Array
-      .range(node.getStartLineNumber, node.getEndLineNumber + 1)
-      .foreach(styledText.setParagraphStyle(_, List(style).asJava))
-  }
 }
