b63c87431c6c67e924dad8b11f73747e03dafd69
raayanpillai <raayan@raayanpillai.com>
2019-11-07 21:55:06 -0500

    scalafmt'd and fixed erroneous paren

--- ci/dist_mac_os.sh
+++ ci/dist_mac_os.sh
@@ -1,8 +1,8 @@
-curl -O https://download.java.net/java/early_access/jpackage/1/openjdk-14-jpackage+1-49_osx-x64_bin.tar.gz
+curl -o jpackage.tar.gz $(curl -v --silent https://jdk.java.net/jpackage/ 2>&1 | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep "jpackage+1-.*osx-x64_bin.tar.gz$")
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 jabba install adopt@1.12.0-2
 jabba use adopt@1.12.0-2
-tar -xzf openjdk-14-jpackage+1-49_osx-x64_bin.tar.gz
+tar -xzf jpackage.tar.gz
 BADASS_RUNTIME_JPACKAGE_HOME=jdk-14.jdk/Contents/Home ./gradlew jpackageImage
 cd build/jpackage
 tar czf lockbook-macos.tar.gz Lockbook.app
