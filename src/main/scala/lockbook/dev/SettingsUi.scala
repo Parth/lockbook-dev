afa83ec8049a156b4ae5a55b2b77cb50abdd3e9d
Smail <SmailBarkouch1@gmail.com>
2020-01-09 12:25:35 -0500

    functional changes to settingsui

--- src/main/scala/lockbook/dev/SettingsUi.scala
+++ src/main/scala/lockbook/dev/SettingsUi.scala
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections
 import javafx.geometry.Insets
 import javafx.scene.control
 import javafx.scene.control.{TextFormatter, _}
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.GridPane
 
 import scala.concurrent.duration.FiniteDuration
@@ -29,6 +30,12 @@ class SettingsUi(settingsHelper: SettingsHelper, fileHelper: FileHelper) {
 
     if (result.isPresent) {
       SettingsHelper.constructJson(result.get(), fileHelper)
+
+      val restartDialog: Dialog[ButtonType] = new Dialog[ButtonType]()
+      restartDialog.getDialogPane.getButtonTypes.addAll(ButtonType.OK)
+      restartDialog.getDialogPane.setContentText("Restart Lockbook for settings to take effect.")
+      restartDialog.getDialogPane.getStylesheets.add(settingsHelper.getTheme.fileName)
+      restartDialog.showAndWait()
     }
   }
 
@@ -36,27 +43,36 @@ class SettingsUi(settingsHelper: SettingsHelper, fileHelper: FileHelper) {
     val stylesBox        = new ComboBox[Theme](FXCollections.observableArrayList(Light))
     val autoLockIntField = new TextField()
     val autoLockCheckBox = new control.CheckBox()
+    val durationLabel    = new Label("Duration:") // included to use id to remove node
 
     stylesBox.getSelectionModel.select(settingsHelper.getTheme) // TODO: Make autolockIntField appear after lockbookCheckBox is unchecked, intuitive
-    if (settingsHelper.getAutoLock.time.isEmpty) {
+
+    if (settingsHelper.getAutoLock.time.isEmpty) autoLockCheckBox.setSelected(false)
+    else {
       autoLockCheckBox.setSelected(true)
-      autoLockIntField.setDisable(true)
-    } else {
       autoLockIntField.setText(settingsHelper.getAutoLock.time.get.toMinutes.toString)
     }
 
     autoLockCheckBox.setOnAction(
-      _ =>
-        autoLockIntField.setDisable(
-          if (autoLockCheckBox.isSelected) true
-          else false
-        )
+      _ => {
+        if (autoLockCheckBox.isSelected) {
+          gridPane.add(durationLabel, 0, 4)
+          gridPane.add(autoLockIntField, 1, 4)
+        }
+
+        if (!autoLockCheckBox.isSelected) {
+          gridPane.getChildren.remove(durationLabel)
+          gridPane.getChildren.remove(autoLockIntField)
+        }
+      }
     )
 
     autoLockIntField.setTextFormatter(new TextFormatter[String]((change: TextFormatter.Change) => {
       change.setText(change.getText.replaceAll("""\D+""", ""))
-      if (autoLockIntField.getText.length > 1 || autoLockIntField.getText.length == 0 && change.getText == "0")
-        change.setText("")
+
+      if (!(change.getText.length == 0 || autoLockIntField.getText.length == 0))
+        if ((autoLockIntField.getText + change.getText).toShortOption.isEmpty || autoLockIntField.getText.length == 0 && change.getText == "0") // use short limit to avoid specific problems from finiteduration preference
+          change.setText("")
 
       change
     }))
@@ -68,16 +84,22 @@ class SettingsUi(settingsHelper: SettingsHelper, fileHelper: FileHelper) {
     gridPane.setHgap(10)
     gridPane.setVgap(10)
     gridPane.setPadding(new Insets(10))
+    gridPane.setMinSize(300, 200)
 
     gridPane.add(new Label("Styles"), 0, 1)
     gridPane.add(stylesBox, 1, 1)
     gridPane.add(new Label("Auto Lock Time"), 0, 2)
-    gridPane.add(autoLockIntField, 1, 2)
-    gridPane.add(autoLockCheckBox, 2, 2)
-    gridPane.add(new Label("Restart to have settings take effect"), 0, 3)
+    gridPane.add(new Label("Enable:"), 0, 3)
+    gridPane.add(autoLockCheckBox, 1, 3)
+
+    if (autoLockCheckBox.isSelected) {
+      gridPane.add(durationLabel, 0, 4)
+      gridPane.add(autoLockIntField, 1, 4)
+    }
 
     dialog.getDialogPane.getStylesheets.add(settingsHelper.getTheme.fileName)
     dialog.getDialogPane.setContent(gridPane)
+
 
     dialog.setResultConverter(
       dialogButton => {
@@ -85,8 +107,8 @@ class SettingsUi(settingsHelper: SettingsHelper, fileHelper: FileHelper) {
           LockbookSettings(
             Some(stylesBox.getValue),
             Some( // can simplify
-              if (autoLockCheckBox.isSelected || autoLockIntField.getText.isEmpty) AutoLock(None)
-              else AutoLock(Some(FiniteDuration(autoLockIntField.getText.toInt, TimeUnit.MINUTES)))
+              if (!autoLockCheckBox.isSelected) AutoLock(None)
+              else AutoLock(Some(FiniteDuration(autoLockIntField.getText.toLong, TimeUnit.MINUTES)))
             )
           )
         else null
