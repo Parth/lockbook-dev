510077032abdcce7fdd8c7b26ea8ad410ddb6be9
Parth <parth@mehrotra.me>
2020-01-16 13:47:30 -0500

    update ci

--- ci/dist_linux.sh
+++ ci/dist_linux.sh
@@ -1,5 +1,5 @@
-curl -o jpackage.tar.gz https://download.java.net/java/early_access/jdk14/31/GPL/openjdk-14-ea+31_linux-x64_bin.tar.gz
-tar -xzf jpackage.tar.gz
-BADASS_RUNTIME_JPACKAGE_HOME=jdk-14 ./gradlew jpackageImage
-cd build/jpackage
-tar czf lockbook-linux.tar.gz Lockbook
+curl -o jpackage.tar.gz https://download.java.net/java/early_access/jdk14/31/GPL/openjdk-14-ea+31_linux-x64_bin.tar.gz && 
+tar -xzf jpackage.tar.gz && 
+BADASS_RUNTIME_JPACKAGE_HOME=jdk-14 ./gradlew jpackageImage &&
+cd build/jpackage &&
+tar czf lockbook-linux.tar.gz Lockbook 
--- ci/dist_mac_os.sh
+++ ci/dist_mac_os.sh
@@ -1,8 +1,5 @@
-curl -o jpackage.tar.gz https://download.java.net/java/early_access/jdk14/31/GPL/openjdk-14-ea+31_osx-x64_bin.tar.gz
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-jabba install adopt@1.12.0-2
-jabba use adopt@1.12.0-2
-tar -xzf jpackage.tar.gz
-BADASS_RUNTIME_JPACKAGE_HOME=jdk-14.jdk/Contents/Home ./gradlew jpackageImage
-cd build/jpackage
-tar czf lockbook-macos.tar.gz Lockbook.app
+curl -o jpackage.tar.gz https://download.java.net/java/early_access/jdk14/31/GPL/openjdk-14-ea+31_osx-x64_bin.tar.gz &&
+tar -xzf jpackage.tar.gz && 
+BADASS_RUNTIME_JPACKAGE_HOME=jdk-14.jdk/Contents/Home ./gradlew jpackageImage && 
+cd build/jpackage &&
+tar czf lockbook-macos.tar.gz Lockbook.app 
