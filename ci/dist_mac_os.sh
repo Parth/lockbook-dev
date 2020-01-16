ff52c6c2f2ea9817615fd59d3ae374682d76bcf0
Parth <parth@mehrotra.me>
2020-01-16 13:24:47 -0500

    fix ci

--- ci/dist_mac_os.sh
+++ ci/dist_mac_os.sh
@@ -1,4 +1,4 @@
-curl -o jpackage.tar.gz $(curl -v --silent https://jdk.java.net/jpackage/ 2>&1 | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep "jpackage+1-.*osx-x64_bin.tar.gz$")
+curl -o jpackage.tar.gz https://download.java.net/java/early_access/jdk14/31/GPL/openjdk-14-ea+31_osx-x64_bin.tar.gz
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 jabba install adopt@1.12.0-2
 jabba use adopt@1.12.0-2
